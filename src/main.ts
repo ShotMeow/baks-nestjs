875819e6f6e67c9ec2d483b2e0804e156c0236e0
ShotMeow <shotmeow@gmail.com>
2024-06-06 12:59:17 +0300

    Remove : /api uri

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.setGlobalPrefix('api');
   await app.listen(3001);
 }
 bootstrap();
