9700d45e7583118e797fd98480b47312a7f66275
ShotMeow <shotmeow@gmail.com>
2024-06-12 05:42:39 +0300

    Test: Migrate to Express

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import {
-  FastifyAdapter,
-  NestFastifyApplication,
-} from '@nestjs/platform-fastify';
-import multipart from '@fastify/multipart';
-import * as path from 'path';
-async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(
-    AppModule,
-    new FastifyAdapter(),
-  );
-  app.useStaticAssets({
-    root: path.join(__dirname, '../storage/images/'),
-    prefix: '/images/',
-  });
 
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.register(multipart);
   await app.listen(3001);
 
   return 3001;
